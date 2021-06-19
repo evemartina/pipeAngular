2bfdc99a06be1716f9c170e237dae5adcb7ebd88
evelyn henriquez <evemartinahm@gmail.com>
2021-06-18 22:32:55 -0400

    primeNG

---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+  ]
+})
+export class MenuComponent implements OnInit {
+  items: MenuItem[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label:'Pipes Angular',
+        icon:'pi pi-Desktop',
+        items:[
+          {
+            label:'Texto y Fechas',
+            icon:'pi pi-aling-left',
+            routerLink:'/'
+          },
+          {
+            label:'Mumeros',
+            icon:'pi pi-dollar',
+            routerLink:'numeros'
+          },
+          {
+            label:'Mumeros',
+            icon:'pi pi-globe',
+            routerLink:'no-comunes'
+          }
+        ]
+      },
+      {
+        label:'Personalizados',
+        icon:'pi pi-cog'
+      }
+    ];
+  }
+}
